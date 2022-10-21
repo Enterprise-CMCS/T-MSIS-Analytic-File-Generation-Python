632b53c8d0e82ee556d476da2d81d97c1d5573e8
Scott Cleeton <scleeton@tensileai.com>
2022-10-21 16:22:58 -0400

    Renamed OT target tables

--- taf/OT/OTL.py
+++ taf/OT/OTL.py
@@ -116,7 +116,7 @@ class OTL:
     def build(self, runner: OT_Runner):
 
         z = f"""
-                INSERT INTO {runner.DA_SCHEMA_DC}.taf_otl_test_srvcs
+                INSERT INTO {runner.DA_SCHEMA_DC}.taf_otl
                 SELECT
                     { OT_Metadata.finalFormatter(OT_Metadata.line_columns) }
                 FROM OTL
