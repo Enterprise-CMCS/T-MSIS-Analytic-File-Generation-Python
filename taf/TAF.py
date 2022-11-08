54a03b7675e0dc7e4eabac02f7dd32d30fabc737
Scott Cleeton <scleeton@tensileai.com>
2022-11-08 13:57:51 -0500

    Updated docstring

--- taf/TAF.py
+++ taf/TAF.py
@@ -1,23 +1,24 @@
-# -----------------------------------------------------------------------------
-#
-#
-#
-# -----------------------------------------------------------------------------
 from taf.TAF_Runner import TAF_Runner
 
 
-class TAF():
+class TAF:
     """
-    TODO:  Update docstring
+    A class to represent a T-MSIS analytic file.
     """
-     
+
     def __init__(self, runner: TAF_Runner):
         """
-        TODO:  Update docstring
+        Constructs all the necessary attributes for the T-MSIS analytic file object.
+
+            Parameters:
+                None
+
+            Returns:
+                None
         """
-         
+
         self.runner = runner
-        self.st_fil_type = 'undefined'
+        self.st_fil_type = "undefined"
 
 
 # -----------------------------------------------------------------------------
