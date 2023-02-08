d584ad2ec82f5fcbaddb31a28528cd30c9a2c509
Scott Cleeton <scleeton@tensileai.com>
2023-02-08 12:33:07 -0500

    Change Notebook extension to be able to see diffs

---
+++ .databricks/notebooks/runners/up_runner.py
@@ -0,0 +1,158 @@
+{
+    "cells": [
+        {
+            "cell_type": "code",
+            "source": ["from taf.UP.UP_Runner import UP_Runner"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "38c2ad3b-c18d-4494-9710-d28bc2994e96",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": [
+                'up = UP_Runner(reporting_period = dbutils.widgets.get("reporting_period")\n              ,state_code       = dbutils.widgets.get("state_code")\n              ,run_id           = dbutils.widgets.get("run_id")\n              ,job_id           = dbutils.widgets.get("job_id"))'
+            ],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "45dd07a1-3aba-433b-8dab-4a9ba55fb3d5",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ['up.job_control_wrt("AUP")'],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "51e02232-10ff-43fe-9cad-ca6c670ec284",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.job_control_updt()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "13c99adb-48d2-4e2d-aba0-d13272b20317",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.print()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "a52d0531-5785-493c-8dcc-4951f6ec6618",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.init()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "27583994-db23-47bb-ac50-a59678dfa017",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.run()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "28c5eb24-72d9-4b2f-b152-3a1339de7c0e",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.view_plan()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "fece35cc-84c4-4ebf-aa04-2be45b081fc3",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["display(up.audit())"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "c08fc73e-c2e0-403a-83ae-fb166d307f9a",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+        {
+            "cell_type": "code",
+            "source": ["up.job_control_updt2()"],
+            "metadata": {
+                "application/vnd.databricks.v1+cell": {
+                    "title": "",
+                    "showTitle": false,
+                    "inputWidgets": {},
+                    "nuid": "1a330971-dfb5-49f6-b6aa-3c431b77152b",
+                }
+            },
+            "outputs": [],
+            "execution_count": 0,
+        },
+    ],
+    "metadata": {
+        "application/vnd.databricks.v1+notebook": {
+            "notebookName": "UP Runner",
+            "dashboards": [],
+            "notebookMetadata": {"pythonIndentUnit": 2},
+            "language": "python",
+            "widgets": {},
+            "notebookOrigID": 508835,
+        }
+    },
+    "nbformat": 4,
+    "nbformat_minor": 0,
+}
