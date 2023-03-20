51c21120f9421e6ba87d3d595cfeb634ed58b04e
Matt Fury <matthew.fury@cms.hhs.gov>
2023-03-20 15:30:13 -0400

    remaining runners that needed file_version

--- taf/LT/LT_Runner.py
+++ taf/LT/LT_Runner.py
@@ -10,8 +10,19 @@ class LT_Runner(TAF_Runner):
     These statements are intended to initialize the module.
     """
 
-    def __init__(self, da_schema: str, reporting_period: str, state_code: str, run_id: str, job_id: int):
-        super().__init__(da_schema, reporting_period, state_code, run_id, job_id)
+    def __init__(self,
+                 da_schema: str,
+                 reporting_period: str,
+                 state_code: str,
+                 run_id: str,
+                 job_id: int,
+                 file_version: str):
+        super().__init__(da_schema,
+                         reporting_period,
+                         state_code,
+                         run_id,
+                         job_id,
+                         file_version)
 
     def init(self):
         """
