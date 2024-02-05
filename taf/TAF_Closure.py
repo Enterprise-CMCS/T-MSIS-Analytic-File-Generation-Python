6895837957606d5c7c1100eb105c622c75b9e1f6
a-pfeiffer <apfeiffer@mathematica-mpr.com>
2024-02-05 09:00:34 -0500

    DBDAART-6418 - Added new valid values to var_set_spclty function in TAF_Closure.py

--- taf/TAF_Closure.py
+++ taf/TAF_Closure.py
@@ -380,7 +380,7 @@ class TAF_Closure:
                 case when ({var} >= 1 and {var} <= 98) then upper(lpad({var}, 2, '0'))
                     else NULL
                 end
-                else case when upper({var}) in ('A0','A1','A2','A3','A4','A5','A6','A7','A8','A9','B1','B2','B3','B4','B5') then upper(lpad({var}, 2, '0'))
+                else case when upper({var}) in ('A0','A1','A2','A3','A4','A5','A6','A7','A8','A9','B1','B2','B3','B4','B5','C0','C1','C2','C3','C4','C5','C6','C7','C8','C9','D1','D2','D3','D4','D5','D6','D7','D8') then upper(lpad({var}, 2, '0'))
                         else NULL
                 end
             end  as {var}
