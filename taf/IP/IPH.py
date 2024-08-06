899e27e406289248d38d68db7fe305b48f2f9bb1
Andrew Pfeiffer <apfeiffer@mathematica-mpr.com>
2024-08-06 08:09:30 -0400

    DBDAART-7238-Add-SRVC_BGNNG_DT

--- taf/IP/IPH.py
+++ taf/IP/IPH.py
@@ -260,7 +260,10 @@ class IPH:
                 ,TOT_BENE_COPMT_LBLE_AMT
                 ,TOT_BENE_COINSRNC_LBLE_AMT
                 ,CMBND_BENE_CST_SHRNG_PD_AMT
-                ,{ TAF_Closure.fix_old_dates('SRVC_BGNNG_DT')}
+                , case
+                    when (SRVC_BGNNG_DT < '1600-01-01') then '1599-12-31'
+                    else nullif(SRVC_BGNNG_DT, '1960-01-01')
+                    end as SRVC_BGNNG_DT
             FROM (
                 select
                     *,
