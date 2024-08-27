6c728106d382716a961ca60bac644db5c9180e6b
Andrew Pfeiffer <apfeiffer@mathematica-mpr.com>
2024-08-27 13:47:05 -0400

    DBDAART-7294-OTH-Add-TOT_BENE_COINSRNC_LBLE_AMT

--- taf/OT/OTH.py
+++ taf/OT/OTH.py
@@ -181,6 +181,7 @@ class OTH:
                 ,{ TAF_Closure.var_set_type1('SRC_LCTN_CD',lpad=2)}
                 ,TOT_BENE_DDCTBL_LBLE_AMT
                 ,TOT_BENE_COPMT_LBLE_AMT
+                ,TOT_BENE_COINSRNC_LBLE_AMT
             from (
                 select
                     *,
