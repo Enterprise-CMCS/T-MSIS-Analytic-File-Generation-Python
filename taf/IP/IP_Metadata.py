a0366717854365de218acde4dceb675ea7d70a2f
a-pfeiffer <apfeiffer@mathematica-mpr.com>
2025-03-28 12:52:31 -0400

    DBDAART-12827 - Removed cast as int from DX sqnc number.

--- taf/IP/IP_Metadata.py
+++ taf/IP/IP_Metadata.py
@@ -104,8 +104,7 @@ class IP_Metadata:
         "IMNZTN_TYPE_CD":TAF_Closure.set_as_null,
         "XIX_SRVC_CTGRY_CD":TAF_Closure.set_as_null,
         "XXI_SRVC_CTGRY_CD":TAF_Closure.set_as_null,
-        "DGNS_CD":TAF_Closure.compress_dots,
-        "DGNS_SQNC_NUM":TAF_Closure.cast_as_int
+        "DGNS_CD":TAF_Closure.compress_dots
     }
 
     validator = {}
